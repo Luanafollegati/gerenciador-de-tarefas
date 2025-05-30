68e21734501594c061ba4c2d55f0f1258578f054
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 11:04:15 -0300

    feat: filtra tarefas

--- lista-metodos-array/tarefas11.js
+++ lista-metodos-array/tarefas11.js
@@ -1,1 +1,6 @@
 // 11
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+let tarefasFiltradas = tarefas.filter(tarefa => tarefa.length > 15);
+
+console.log(`Tarefas filtradas (tarefas que têm mais de 15 caracteres): ${tarefasFiltradas}`);
