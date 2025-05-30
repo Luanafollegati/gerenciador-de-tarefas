be2e12656ad34822057251a875312704259f4174
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 08:49:54 -0300

    feat: Remove última tarefa

--- lista-metodos-array/tarefas02.js
+++ lista-metodos-array/tarefas02.js
@@ -1,1 +1,12 @@
 // 02
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+console.log("antes do pop");
+console.table(tarefas);
+
+// removendo a ultima tarefa
+let estiloRemovido = tarefas.pop();
+
+console.log("depois do pop");
+console.table(tarefas);  
+
+console.log("estilo removido: ", estiloRemovido);
