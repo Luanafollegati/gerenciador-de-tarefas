d6fff120f395e167ca81550c4144340b284ba85b
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 09:12:33 -0300

    feat: Verifica existência de tarefa

--- lista-metodos-array/tarefas05.js
+++ lista-metodos-array/tarefas05.js
@@ -1,1 +1,4 @@
 // 05
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log(`A playlist inclui 'Ir à academia'? ${tarefas.includes("Ir à academia")} `);
