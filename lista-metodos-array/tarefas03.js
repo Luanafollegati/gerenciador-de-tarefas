ca9a2ec2b603df6dd5c33947b480f1f9c76acda7
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 08:54:26 -0300

    feat: Adiciona tarefa no início

---
+++ lista-metodos-array/tarefas03.js
@@ -0,0 +1,9 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+console.log("antes do unshift");
+console.table(tarefas);
+
+//Adicionando uma nova tarefa no inicio da lista
+tarefas.unshift("Comer chocolate")
+
+console.log("depois do unshift");
+console.table(tarefas);
