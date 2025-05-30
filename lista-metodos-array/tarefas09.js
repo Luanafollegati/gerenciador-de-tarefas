689dca8b0ff0ca05da4b6731677434149fef6ef1
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 10:35:12 -0300

    feat: Concatena listas de tarefas

--- lista-metodos-array/tarefas09.js
+++ lista-metodos-array/tarefas09.js
@@ -1,1 +1,7 @@
 // 09
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+let tarefas2 = ['comer chocolate', 'dançar samba', 'gravar tiktok'];
+
+// juntando com a lista inicial
+console.log("tarefas combinadas:", tarefas.concat(tarefas2));
+
