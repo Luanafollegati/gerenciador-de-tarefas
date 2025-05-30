090a39e4162695fd93c9466508688cfe9fe401a8
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 08:58:01 -0300

    feat: Cria arquivo tarefas04.js dentro da pasta lista-metodos-array

--- lista-metodos-array/tarefas03.js
+++ lista-metodos-array/tarefas03.js
@@ -1,4 +1,7 @@
+// 03
+
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
 console.log("antes do unshift");
 console.table(tarefas);
 
