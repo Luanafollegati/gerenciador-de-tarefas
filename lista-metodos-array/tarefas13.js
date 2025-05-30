aa8ad7567cbda8cd39fe111317ff120389ed6e62
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 11:16:08 -0300

    feat: Inverta a Ordem das Tarefas

--- lista-metodos-array/tarefas13.js
+++ lista-metodos-array/tarefas13.js
@@ -1,1 +1,6 @@
 //13
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+let tarefasInvertidas = tarefas.reverse();
+
+//Invertendo a Ordem das Tarefas
+console.log("Tarefas invertidas:", tarefasInvertidas);
