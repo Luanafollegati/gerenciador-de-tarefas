4c37d658f292713b7fae7d342d8117383a67a598
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 09:17:03 -0300

    feat: Transfor a lista de tarefas em string

--- lista-metodos-array/tarefas06.js
+++ lista-metodos-array/tarefas06.js
@@ -1,1 +1,4 @@
 // 06
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log("Tarefas como string:", tarefas.join(", "));
