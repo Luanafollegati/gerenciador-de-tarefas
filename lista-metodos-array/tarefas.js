a26d5be324b9f55e2398e638e7d462ffabd316d4
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 08:43:28 -0300

    feat: Adiciona novas tarefas

--- lista-metodos-array/tarefas.js
+++ lista-metodos-array/tarefas.js
@@ -1,1 +1,10 @@
 // 01
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+console.log("antes do push");
+console.table(tarefas);
+
+// adicionando novas tarefas
+tarefas.push("Comer chocolate", "Dançar samba");
+
+console.log("depois do push");
+console.table(tarefas);
