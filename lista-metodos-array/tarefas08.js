9b975a2c9fa071e1d952417f97d356ab321bbb8c
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 09:58:09 -0300

    feat: Altera lista

--- lista-metodos-array/tarefas08.js
+++ lista-metodos-array/tarefas08.js
@@ -1,1 +1,14 @@
 //08
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log("Antes do splice:");
+console.table(tarefas);
+
+// Removendo a tarefa no índice 3 e substituindo por uma nova tarefa.
+let tarefasRemovidas = tarefas.splice(3, 1);
+
+console.log("Depois do splice:");
+tarefas.splice(3,0, "Comer chocolate");
+console.table(tarefas);
+
+console.log("Tarefas removidas:", tarefasRemovidas);
